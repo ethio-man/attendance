22d9ab4514458e120d3a713a2f964189792f40e6
Dagmawi Antehun <natymiskir@gmail.com>
2025-12-23 09:44:38 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173','https://frontend-seven-sable-29.vercel.app/'],
     credentials:true
   });
 
